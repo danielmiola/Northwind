f3d1ee6a1e6ac8d1ed54261285a9ea54180d135f
danielmiola <daniel.miola@hotmail.com>
2015-11-12 16:09:03 -0200

    Initial commit. Tons of code already done.

---
+++ Northwind/Controllers/RegionController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class RegionController : Controller
+    {
+        private NorthwindEntities db = new NorthwindEntities();
+
+        // GET: /Region/
+        public ActionResult Index()
+        {
+            return View(db.Region.ToList());
+        }
+
+        // GET: /Region/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Region region = db.Region.Find(id);
+            if (region == null)
+            {
+                return HttpNotFound();
+            }
+            return View(region);
+        }
+
+        // GET: /Region/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Region/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="RegionID,RegionDescription")] Region region)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Region.Add(region);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(region);
+        }
+
+        // GET: /Region/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Region region = db.Region.Find(id);
+            if (region == null)
+            {
+                return HttpNotFound();
+            }
+            return View(region);
+        }
+
+        // POST: /Region/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="RegionID,RegionDescription")] Region region)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(region).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(region);
+        }
+
+        // GET: /Region/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Region region = db.Region.Find(id);
+            if (region == null)
+            {
+                return HttpNotFound();
+            }
+            return View(region);
+        }
+
+        // POST: /Region/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Region region = db.Region.Find(id);
+            db.Region.Remove(region);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
